e6058407a306767a3ac8c4781971204550251f64
natalia <n.cuelloitecriocuarto.org.ar>
2025-11-09 18:37:31 -0300

    cambios de correccion

--- models/user.js
+++ models/user.js
@@ -5,20 +5,15 @@ module.exports = (sequelize, DataTypes) => {
     {
       id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 
-      // nombres de usuario (viejo y nuevo)
-      username:   { type: DataTypes.STRING, allowNull: true, field: "username" },
+ 
       nombre:     { type: DataTypes.STRING, allowNull: true, field: "nombre" },
 
-      // correo (nuevo)
+    
       correo:     { type: DataTypes.STRING, allowNull: true, field: "correo", unique: true },
 
-      // CONTRASEÑAS (ambas columnas, importante!)
-      password:   { type: DataTypes.STRING, allowNull: true, field: "password" },      // vieja
-      contraseña: { type: DataTypes.STRING, allowNull: true, field: "contraseña" },    // nueva
+      contraseña: { type: DataTypes.STRING, allowNull: true, field: "contraseña" },   
 
-      // ROLES (ambas columnas)
-      role: { type: DataTypes.ENUM("cliente","empleado","admin"), allowNull: true, field: "role" }, // viejo
-      rol:  { type: DataTypes.ENUM("cliente","empleado","admin"), allowNull: true, field: "rol"  }, // nuevo
+      rol:  { type: DataTypes.ENUM("cliente","empleado","admin"), allowNull: true, field: "rol"  }, 
 
       is_active:  { type: DataTypes.BOOLEAN, allowNull: true, field: "is_active", defaultValue: 1 },
     },
